227510e16717bdd083fdec06c218012c8b488fb4
mustafa demirtaş <mustafademirtas94@gmail.com>
2022-08-08 20:58:10 +0300

    admin panel arayüz

---
+++ AspNetCoreWebApplication/Areas/Admin/Controllers/AppUsersController.cs
@@ -0,0 +1,124 @@
+﻿using AspNetCoreWebApplication.Data;
+using AspNetCoreWebApplication.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AppUsersController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public AppUsersController(DatabaseContext context)
+        {
+            _databaseContext = context;
+        }
+
+        // GET: AppUsersController
+        public async Task<ActionResult> IndexAsync()
+        {
+            return View(await _databaseContext.AppUsers.ToListAsync());
+        }
+
+        // GET: AppUsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AppUsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AppUsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                   await _databaseContext.AppUsers.AddAsync(appUser);
+                   await _databaseContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştur");
+                }
+
+            }
+            return View(appUser);
+          
+        }
+
+        // GET: AppUsersController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+           var kullanici= await _databaseContext.AppUsers.FindAsync(id);
+
+            return View(kullanici);
+        }
+
+        // POST: AppUsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(AppUser appUser)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                   
+                        _databaseContext.Entry(appUser).State = EntityState.Modified;
+                        await _databaseContext.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    
+                   
+                    
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata oluştur");
+                }
+            }
+            return View(appUser);
+           
+        }
+
+        // GET: AppUsersController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var kullanici = await _databaseContext.AppUsers.FindAsync(id);
+
+            return View(kullanici);
+        }
+
+        // POST: AppUsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteAsync(AppUser appUser)
+        {
+           
+                try
+                {
+                    _databaseContext.AppUsers.Remove(appUser);
+                    //_databaseContext.Entry(appUser).State = EntityState.Deleted;
+                    await _databaseContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    
+                }
+            
+            return View(appUser);
+
+        }
+    }
+}
---
+++ AspNetCoreWebApplication/Areas/Admin/Controllers/BrandsController.cs
@@ -0,0 +1,101 @@
+﻿using AspNetCoreWebApplication.Data;
+using AspNetCoreWebApplication.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandsController : Controller
+    {
+       
+        private readonly DatabaseContext _contex;
+        public BrandsController(DatabaseContext context)
+        {
+            _contex = context;
+        }
+        // GET: BrandsController1
+        public async Task<ActionResult> IndexAsync()
+        {
+            return View(await _contex.Brands.ToListAsync()); 
+        }
+
+        // GET: BrandsController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BrandsController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BrandsController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _contex.Entry(brand).State = EntityState.Added;
+                    //_contex.Brands.Add(brand);
+                   await _contex.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    
+                }
+            }
+            return View(brand);
+
+        }
+
+        // GET: BrandsController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BrandsController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BrandsController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
